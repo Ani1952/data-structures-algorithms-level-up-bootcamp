eabfec9289ee4d8f622acfd9f428bb30fc1582e5
Ani1952 <heroda02@gmail.com>
2021-10-26 20:41:45 +0530

    Update 06 sort_strings.cpp

--- 02 Strings/06 sort_strings.cpp
+++ 02 Strings/06 sort_strings.cpp
@@ -1,58 +1,46 @@
-/*
-Sample Input
-------------
-3
-92 022
-82 12
-77 13
-2 false numeric
 
-Sample Output
--------------
-82 12
-77 13
-92 022
-
-*/
-#include<iostream>
-#include<cstring>
-#include<vector>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-string extractStringAtKey(string str,int key){
+string extractStringAtKey(string str, int key)
+{
 
-	//string tokeniser 
-	// hello how are you
+	// string tokeniser
+	//  hello how are you
 
-	char *s = strtok( (char *)str.c_str(), " ");
-	while(key>1){
-		s = strtok(NULL," ");
+	char *s = strtok((char *)str.c_str(), " ");
+	while (key > 1)
+	{
+		s = strtok(NULL, " ");
 		key--;
 	}
 	return (string)s;
 }
 
-int convertToInt(string s){
+int convertToInt(string s)
+{
 
 	int ans = 0;
 	int p = 1;
 
-	for(int i=s.length()-1; i>=0;i--){
-		ans += ((s[i]-'0')*p);
-		p = p*10;
+	for (int i = s.length() - 1; i >= 0; i--)
+	{
+		ans += ((s[i] - '0') * p);
+		p = p * 10;
 	}
 	return ans;
 }
 
-bool lexicoCompare(pair<string,string> s1, pair<string,string> s2){
+bool lexicoCompare(pair<string, string> s1, pair<string, string> s2)
+{
 	string key1, key2;
 	key1 = s1.second;
 	key2 = s2.second;
 	return key1 < key2;
 }
 
-bool numericCompare(pair<string,string> s1, pair<string,string> s2){
+bool numericCompare(pair<string, string> s1, pair<string, string> s2)
+{
 	string key1, key2;
 	key1 = s1.second;
 	key2 = s2.second;
@@ -60,63 +48,46 @@ bool numericCompare(pair<string,string> s1, pair<string,string> s2){
 	return convertToInt(key1) < convertToInt(key2);
 }
 
-
-
-int main(){
-	string s ="20 30 40 50";
-	
-	
-	int n;
-	cin>>n;
-
-	cin.get(); //consume the extra \n
-
-	string temp;
+int main()
+{
+	int n = 3;
 	vector<string> v;
-	for(int i=0;i<n;i++){
-		getline(cin,temp); //includes white spaces 
-		v.push_back(temp);
-	}
+
+	v={"20,12","45,87","15,17"};
 
 	int key;
-	string reversal,ordering;
-	cin >>key >> reversal >> ordering; 
+	string reversal, ordering;
+	cin >> key >> reversal >> ordering;
 
+	// 1. To extract keys for comparison & store them
+	vector<pair<string, string>> vp;
 
-	//1. To extract keys for comparison & store them
-	vector<pair<string,string> > vp;  
-
-	for(int i=0;i<n;i++){
-		vp.push_back({v[i], extractStringAtKey(v[i],key)});
+	for (int i = 0; i < n; i++)
+	{
+		vp.push_back({v[i], extractStringAtKey(v[i], key)});
 	}
 
-	//2. Sorting 
-	if(ordering=="numeric"){
-		sort(vp.begin(),vp.end(),numericCompare);
+	// 2. Sorting
+	if (ordering == "numeric")
+	{
+		sort(vp.begin(), vp.end(), numericCompare);
 	}
-	else{
-		sort(vp.begin(),vp.end(),lexicoCompare);
-	}
-
-
-	//3. reversal
-	if(reversal=="true"){
-		reverse(vp.begin(),vp.end());
+	else
+	{
+		sort(vp.begin(), vp.end(), lexicoCompare);
 	}
 
-	//4. output
-	for(int i=0;i<n;i++){
-		cout<< vp[i].first << endl;
+	// 3. reversal
+	if (reversal == "true")
+	{
+		reverse(vp.begin(), vp.end());
 	}
 
-
+	// 4. output
+	for (int i = 0; i < n; i++)
+	{
+		cout << vp[i].first << endl;
+	}
 
 	return 0;
 }
-
-
-
-
-
-
-
